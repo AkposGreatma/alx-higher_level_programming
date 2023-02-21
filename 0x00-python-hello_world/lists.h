f14eef57630063270058ecda50f509426236ef54
AkposGreatma <oghenemarogreat77@gmail.com>
2023-02-21 19:29:59 +0000

    checking if a circle is in linked list

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,6 +1,5 @@
-fndef LISTS_H
+#ifndef LISTS_H
 #define LISTS_H
-
 #include <stdlib.h>
 
 /**
@@ -9,12 +8,12 @@ fndef LISTS_H
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
+ * 
  */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
